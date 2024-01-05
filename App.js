b4bb661f44c49cfe408e9353545e61994979605c
unknown <sumantsunny63@gmail.com>
2024-01-05 16:42:59 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,39 @@
+/* const heading = React.createElement("p",
+                                     {id : "heading"},
+                                     "Hello Everyone! from p tag from App.js"
+                                     );
+
+ console.log(heading);    //object
+
+ const names = ReactDOM.createRoot(document.getElementById("root"));
+ names.render(heading); */
+
+//  <div id="parent">
+//     <div id="child">
+//         <h1>I'm h1 tag</h1>
+//         <h2>I'm h1 tag</h2>
+//     </div>
+//    <div id="child2">
+//     <h1>I'm h1 tag</h1>
+//     <h2>I'm h1 tag</h2>
+//  </div>
+//  </div>
+
+ const parent  = React.createElement(
+                "div",
+                {id: "parent"},
+                [React.createElement(
+                    "div",
+                    {id: "child"},
+                    [React.createElement("h1",{},"I'm h1 tag"), React.createElement("h2",{},"I'm h2 tag")]
+                ), 
+                React.createElement(
+                    "div",
+                    {id: "child"},
+                    [React.createElement("h1",{},"I'm h1 tag"), React.createElement("h2",{},"I'm h2 tag")]
+                )  ]   
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
